8392779230fa6d4d0d0fd2c89e1d998d73501e10
Yoni Ifrah <yonifrah996@gmail.com>
2022-09-25 17:59:10 +0300

    added assets

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,8 +4,8 @@ import {FiGithub, FiLinkedin} from 'react-icons/fi'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="https://www.linkedin.com" target="_blank"><FiLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FiGithub/></a>
+        <a href="https://www.linkedin.com/in/yoni-ifrah" target="_blank"><FiLinkedin/></a>
+        <a href="https://github.com/YoniIfrah" target="_blank"><FiGithub/></a>
 
     </div>
   )
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import './portfolio.css'
+import btc from '../../assets/svgtopng/btc.png'
+import covid from '../../assets/svgtopng/covid.png'
+import house from '../../assets/svgtopng/house.png'
+import stocks from '../../assets/svgtopng/stocks.png'
+import webdev from '../../assets/svgtopng/webdev.png'
+
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+      <PortfolioCard   img={btc}  />
+      <PortfolioCard   img={covid}  />
+      <PortfolioCard   img={house}  />
+      <PortfolioCard   img={stocks}  />
+      <PortfolioCard   img={webdev}  />
+      </div>
+
+
+
+    </section>
   )
 }
 
-export default Portfolio
+
+function PortfolioCard(props) {
+  const img=props.img;
+  return (<article className='portfolio_item'>
+      <div className='portfolio_item-image'>
+        <img src={img} alt="" />
+      </div>
+      <h3>This is a portfolio title</h3>
+      <a href="https://github.com" className='btn'>Github</a>
+      <a href="https://github.com" className='btn btn-primary'>Live Demo</a>
+    </article>);
+}
+  export default Portfolio
