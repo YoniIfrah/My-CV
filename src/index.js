7196241a7a38d6e0c39f08b2a9222a0c91cd328d
Yoni Ifrah <yonifrah996@gmail.com>
2022-09-29 17:06:10 +0300

    added dark mode toggle (didnt selected color yet)

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.querySelector("#root"));
+const root = createRoot(document.getElementById("root"));
 root.render(<App/>)
