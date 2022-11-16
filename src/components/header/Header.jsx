dd71c4712c6816a3aaafeb83699ff1b776de843f
Yoni Ifrah <yonifrah996@gmail.com>
2022-11-16 12:58:10 +0200

    fixed bugs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Yoni Ifrah</h1>
-        <h5 className='text-light'>DevOps Engineer</h5>
+        <h5 className='text-light'>FullStack Developer</h5>
         <CTA/>
         <HeaderSocials/>
         <div className='me'>
